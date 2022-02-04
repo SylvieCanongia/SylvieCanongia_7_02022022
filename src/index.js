832d2da34643b1ea61374cb095887c457d444c72
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-02-04 11:43:52 +0100

    refactor: added import of bootstrap styles + logo shaping

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 // Import all Bootstrap's JavaScript plugins
 import 'bootstrap/dist/js/bootstrap.min';
+
+// Import just what we need
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
