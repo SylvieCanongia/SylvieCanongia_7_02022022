88538fef263c16ca6cbf3a0fa305d2b9d64e0653
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-02-21 13:20:19 +0100

    feat: update of dropdown placeholders on click or loss of focus via js

--- scripts/components/dropdowns.js
+++ scripts/components/dropdowns.js
@@ -1,7 +1,6 @@
 import { dropdownItem } from "../templates/dropdownsItem";
 
 const dropdowns = (recipesData) => {
-  console.log(recipesData);
   let ingredients = [];
   let appliance = [];
   let ustensils = [];
@@ -9,6 +8,24 @@ const dropdowns = (recipesData) => {
   const applianceListContainer = document.querySelector('[data-appliance-list]');
   const ustensilsListContainer = document.querySelector('[data-ustensils-list]');
 
+  // Change the text of the placeholders when dropdowns are expanded or not
+  const dropdownsInputs = document.querySelectorAll('[aria-expanded]');
+  
+  for (const input of dropdownsInputs) {
+    // Change the placeholder when a dropdown expands on click
+    input.addEventListener('click', (event) => {
+      const defaultPlaceholder = event.currentTarget.dataset.placeholder;
+      const placeholderExpanded = event.currentTarget.dataset.placeholderExpanded;
+      event.currentTarget.ariaExpanded == 'true' ? event.currentTarget.setAttribute('placeholder', `Rechercher un ${placeholderExpanded}`) : event.currentTarget.setAttribute('placeholder', defaultPlaceholder);
+    });
+
+    // Set the default placeholder when the dropdown loses the focus.
+    input.addEventListener('blur', (event) => {
+      const defaultPlaceholder = event.currentTarget.dataset.placeholder;
+      event.currentTarget.setAttribute('placeholder', defaultPlaceholder);
+    });
+  }
+  
   // Reset the lists of the dropdowns to push new sorted items
   ingredientsListContainer.innerHTML = '';
   applianceListContainer.innerHTML = '';
@@ -16,7 +33,6 @@ const dropdowns = (recipesData) => {
 
   for (let i = 0; i < recipesData.length; i++) {
     for (const ingredientElement of recipesData[i].ingredients) {
-      console.log(!ingredients.includes(ingredientElement.ingredient));
       if (!ingredients.includes(ingredientElement.ingredient)) {
         ingredients.push(ingredientElement.ingredient);
         dropdownItem(ingredientElement.ingredient, "[data-ingredients-list]");
