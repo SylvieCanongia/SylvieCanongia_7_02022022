3f51d0966d4ec7b9c2fd8dc98bc78b5fbf4196ae
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-03-10 12:33:03 +0100

    style: updated some comments or js doc

--- scripts/components/search.js
+++ scripts/components/search.js
@@ -7,21 +7,21 @@ import { tagSearch } from './tagSearch';
  * @param { array } recipes An array of objects containing the data for recipes
  * @param { function } recipeCard Displays all the recipes from the data of the 'recipes'
  *
- * Filters the recipes from the main search box and displays them. Filtering is done by comparing with the name,
- * the ingredients or the description of the recipes data.
+ * Filters the recipes from the main search box and displays them.
+ * Filtering is done by comparing the entered value with the name,
+ * ingredients and description of each recipe.
  */
 const search = (recipes) => {
   const searchInputElement = document.querySelector("[data-search]");
 
   /**
-   *
+   * See if the recipe isn't already in the array recipesToDisplay to avoid duplication
+   * if no, push the recipe into the array
    * @param { array } recipes Array from which are taken the data of the recipes via their id
    * @param { array } recipesToDisplay Recipes to display after sorting of the recipes from the main Search
    * @param { number } indexOfRecipeToDisplay The index of the recipe to display
    */
   const pushIfNoDuplicate = (recipes, recipesToDisplay, indexOfRecipeToDisplay) => {
-    // See if the recipe isn't already in the array recipesToDisplay to avoid duplication
-    // if no, push the recipe into the array
     if (!recipesToDisplay.includes(recipes[indexOfRecipeToDisplay])) {
       recipesToDisplay.push(recipes[indexOfRecipeToDisplay]);
     }
@@ -51,9 +51,8 @@ const search = (recipes) => {
     delete recipesData[i].ustensils;
   }
 
-  console.log(recipesData)
-
   // Concatenete all the name, ingredients and description contents
+  // Then returns each recipe with its id and its concateneted content
   const recipesConcat = recipesData.map((recipe) => {
 
     // 1. Gives access to each ingredient in the 'ingredients' property
@@ -71,25 +70,28 @@ const search = (recipes) => {
   // The items of the dropdowns are also updated at the same time
   searchInputElement.addEventListener("input", (event) => {
 
-    // Sets uppercase characters to lowercase and remove accents / diacritics
+    // Sets the entered value to lowercase and remove accents / diacritics
     const value = event.target.value
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "");
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
     let arrayOfValues = value.split(" ");
 
     // empty the array to reset the displayed recipes
     recipesToDisplay = [];
 
     if (value.length >= 3) {
+      // Loops on each recipe
       Object.entries(recipesConcat).forEach(recipe => {
 
         // Loop on each value of the arrayOfValues to see if the value is
-        // in the recipe's name, ingredients or description
-        arrayOfValues.forEach( (valueElement) => {
+        // in the recipe's concateneted name, ingredients and description
+        arrayOfValues.forEach((valueElement) => {
           if (valueElement !== "") {
             const isVisible = recipe[1].stringToCheck.includes(valueElement);
-            if(isVisible) {
+
+            //  If the value is in the recipe, find the corresponding recipe with the id
+            if (isVisible) {
               const indexOfRecipeToDisplay = recipes.findIndex(
                 (el) => el.id == recipe[1].id
               );
@@ -102,10 +104,8 @@ const search = (recipes) => {
         })
       })
 
-      // The array of recipes for the search by tag is updated
+      // The array of recipes to memorize for the search by tag is updated
       let mainSearchRecipesToDisplay = [...recipesToDisplay];
-
-      // console.log(recipesToDisplay, mainSearchRecipesToDisplay)
 
       // Creates the cards of the filtered recipes and displays them
       recipeCard(recipesToDisplay);
@@ -114,6 +114,7 @@ const search = (recipes) => {
       dropdowns(recipesToDisplay);
 
       tagSearch(recipesToDisplay, mainSearchRecipesToDisplay);
+
     } else {
       // if entered value is < 3 chars, creates cards and displays all the recipes
       recipeCard(recipes);
